727f9df485790688af93d7b943ee47a6c3bf49da
rendyelang <lsmnrndy123@gmail.com>
2025-12-15 06:54:04 +0700

    fix: add missing semicolon in ownerAuthRouter

--- src/routes/ownerAuthRouter.js
+++ src/routes/ownerAuthRouter.js
@@ -3,6 +3,6 @@ const { addAdmin, signInOwner } = require('../controllers/ownerAuthController');
 const router = require('express').Router();
 
 router.post('/owner/sign-up', addAdmin);
-router.post('/owner/sign-in', signInOwner)
+router.post('/owner/sign-in', signInOwner);
 
 module.exports = router;
