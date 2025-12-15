1c9096a1afd8347499acfd705828477690e04c28
rendyelang <lsmnrndy123@gmail.com>
2025-12-15 09:11:30 +0700

    feat: add additional room management routes for CRUD operations

--- src/routes/roomRouter.js
+++ src/routes/roomRouter.js
@@ -1,8 +1,18 @@
 const router = require('express').Router()
-const { addRoom } = require('../controllers/roomController')
+const { addRoom, getRooms, getRoomsByOwnerId, editRoom, editRoomStatus, deleteRoom, getRoomById } = require('../controllers/roomController')
 const multerMiddleware = require('../middlewares/multerMiddleware')
 const protectedToken = require('../middlewares/verifyAccessTokenMiddleware')
+const { route } = require('./ownerAuthRouter')
 
 router.post('/rooms/add-room', protectedToken, multerMiddleware('rooms'), addRoom)
+router.get('/rooms', getRooms)
+router.get('/room/:id', protectedToken, getRoomById)
+
+// Get rooms for the logged-in owner
+router.get('/rooms/me', protectedToken, getRoomsByOwnerId)
+
+router.put('/rooms/edit/:id', protectedToken, multerMiddleware('rooms'), editRoom)
+router.patch('/room/edit-status/:id', protectedToken, editRoomStatus)
+router.delete('/rooms/delete/:id', protectedToken, deleteRoom)
 
 module.exports = router
