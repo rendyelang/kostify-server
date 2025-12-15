693cdb0d4ac6b7630de25414fa1d63956c930b1e
rendyelang <lsmnrndy123@gmail.com>
2025-12-15 09:11:04 +0700

    feat: add CRUD operations for room management

--- src/models/rooms.js
+++ src/models/rooms.js
@@ -19,6 +19,64 @@ const createRoom = async (room) => {
     return newRoom
 }
 
+// Get all rooms
+const getRooms = async () => {
+    const rooms = await prisma.rooms.findMany()
+    return rooms
+}
+
+// Get all rooms by owner ID
+const getRoomsbyOwnerId = async (owner_id) => {
+    const rooms = await prisma.rooms.findMany({
+        where: {
+            owner_id: owner_id
+        }
+    })
+    return rooms
+}
+
+// Get room by ID
+const getRoomById = async (roomId) => {
+    const room = await prisma.rooms.findUnique({
+        where: { room_id: Number(roomId) }
+    })
+    return room
+}
+
+// Edit room details
+const editRoom = async (roomId, updatedDetails) => {
+    const updatedRoom = await prisma.rooms.update({
+        where: { 
+            room_id: Number(roomId)
+        },
+        data: updatedDetails
+    })
+    return updatedRoom
+}
+
+// Edit room status
+const editRoomStatus = async (roomId, status) => {
+    const updatedRoom = await prisma.rooms.update({
+        where: { room_id: Number(roomId) },
+        data: { status }
+    })
+    return updatedRoom
+}
+
+// Delete room
+const deleteRoom = async (roomId) => {
+    const deletedRoom = await prisma.rooms.delete({
+        where: { room_id: Number(roomId) }
+    })
+    return deletedRoom
+}
+
 module.exports = {
-    createRoom
+    createRoom, 
+    getRooms,
+    getRoomsbyOwnerId, 
+    editRoom,
+    editRoomStatus,
+    deleteRoom,
+    getRoomById
 }
