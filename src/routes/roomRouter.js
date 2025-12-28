fb493ac4ea43987e0ffa98d83aa4b3541b74eddd
rendyelang <lsmnrndy123@gmail.com>
2025-12-28 22:03:37 +0700

    feat: add getAvailableRoomsByOwner route to roomRouter

--- src/routes/roomRouter.js
+++ src/routes/roomRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { addRoom, getRooms, getRoomsByOwnerId, editRoom, editRoomStatus, deleteRoom, getRoomById } = require('../controllers/roomController')
+const { addRoom, getRooms, getRoomsByOwnerId, editRoom, editRoomStatus, deleteRoom, getRoomById, getAvailableRoomsByOwner } = require('../controllers/roomController')
 const multerMiddleware = require('../middlewares/multerMiddleware')
 const protectedToken = require('../middlewares/verifyAccessTokenMiddleware')
 const { route } = require('./ownerAuthRouter')
@@ -14,5 +14,6 @@ router.get('/rooms/me', protectedToken, getRoomsByOwnerId)
 router.put('/rooms/edit/:id', protectedToken, multerMiddleware('rooms'), editRoom)
 router.patch('/room/edit-status/:id', protectedToken, editRoomStatus)
 router.delete('/rooms/delete/:id', protectedToken, deleteRoom)
+router.get('/rooms/available/me', protectedToken, getAvailableRoomsByOwner)
 
 module.exports = router
