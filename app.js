7e00d6b5ba93675e824fa97147f4b86c16c32252
rendyelang <lsmnrndy123@gmail.com>
2025-12-28 22:03:58 +0700

    feat: add tenant router to main application for tenant management functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const owner = require('./src/routes/ownerAuthRouter');
 const room = require('./src/routes/roomRouter');
+const tenant = require('./src/routes/tenantRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.get('/api', (req, res) => {
 
 app.use('/api', owner);
 app.use('/api', room);
+app.use('/api', tenant);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
