8fe80ed261e2b7ce14b4f8a7ed137e30a13205a5
rendyelang <lsmnrndy123@gmail.com>
2026-01-19 11:53:44 +0700

    feat: add route for retrieving full rooms by owner ID

--- src/routes/roomRouter.js
+++ src/routes/roomRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { addRoom, getRooms, getRoomsByOwnerId, editRoom, editRoomStatus, deleteRoom, getRoomById, getAvailableRoomsByOwner } = require('../controllers/roomController')
+const { addRoom, getRooms, getRoomsByOwnerId, editRoom, editRoomStatus, deleteRoom, getRoomById, getAvailableRoomsByOwner, getFullRoombyOwner } = require('../controllers/roomController')
 const multerMiddleware = require('../middlewares/multerMiddleware')
 const protectedToken = require('../middlewares/verifyAccessTokenMiddleware')
 const { route } = require('./ownerAuthRouter')
@@ -15,5 +15,6 @@ router.put('/rooms/edit/:id', protectedToken, multerMiddleware('rooms'), editRoo
 router.patch('/room/edit-status/:id', protectedToken, editRoomStatus)
 router.delete('/rooms/delete/:id', protectedToken, deleteRoom)
 router.get('/rooms/available/me', protectedToken, getAvailableRoomsByOwner)
+router.get('/rooms/full/me', protectedToken, getFullRoombyOwner)
 
 module.exports = router
